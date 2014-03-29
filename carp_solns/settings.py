1623f0f5681b3755b46c7a3ae28b63b783c04663
Axel Magnuson <axelmagn@gmail.com>
2014-03-29 15:29:24 -0600

    fix static files maybe

--- carp_solns/settings.py
+++ carp_solns/settings.py
@@ -186,7 +186,7 @@ STATIC_URL = "/static/"
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = PROJECT_ROOT + 'staticfiles/'
 
 STATICFILES_DIRS = (
         os.path.join(PROJECT_ROOT, 'static'),
